41051212395231a1d4fe7378cdd30a7e3f9a9f32
btow <btow@yandex.ru>
2017-05-10 19:07:11 +0500

    With handler of menus item

--- app/src/main/java/com/example/samsung/p1141_supportlibrary/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1141_supportlibrary/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.samsung.p1141_supportlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyFragment myFragment = new MyFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.flCont, myFragment).commit();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(R.string.settings);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String message = "The item with ID = ";
+
+        switch (item.getItemId()) {
+            case 0 :
+                Toast.makeText(getBaseContext(), message + "0 selected", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/example/samsung/p1141_supportlibrary/MyFragment.java
@@ -0,0 +1,28 @@
+package com.example.samsung.p1141_supportlibrary;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyFragment extends Fragment {
+
+
+    public MyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, null);
+    }
+
+}
